197edfa1f8b625890e8267748c3d78d02acb56f3
Mia Atkinson <Mia.E.Atkinson@gmail.com>
2018-03-04 20:37:52 -0800

    Katara panel page completed

--- js/slidepanel.js
+++ js/slidepanel.js
@@ -1,24 +1,20 @@
 var urls=[];
 var numPages;
 var slideIndex;
+var pageSize = 9;
 
-function populateURL() {
-    var pageSize = 9;
-    for (i = 0; i < 9; i++) {
-        urls[i] = "https://s3.amazonaws.com/neil-life/Katara/IMG_0821-2.jpg";
-    }
-    for (i = 9; i < 18; i++) {
-        urls[i] = "https://s3.amazonaws.com/neil-life/Katara/Sephora2.jpg";
-    }
-    urls[18] = "https://d3qe819fh6emdz.cloudfront.net/Resources/adventures.jpg";
-    var numPages = Math.ceil(urls.length / pageSize);
-    return numPages;
-}
+var bucketRegion = 'us-east-1';
+var IdentityPoolId = 'us-east-1:d7fe84e1-7d9d-4171-8eb5-334d2e277891';
 
+AWS.config.update({
+    region: bucketRegion,
+    credentials: new AWS.CognitoIdentityCredentials({
+        IdentityPoolId: IdentityPoolId
+    })
+});
 
 // Next/previous controls
 function plusSlides(n) {
-    console.log("plusslides one");
     showSlides(slideIndex += n);
 }
 
@@ -26,7 +22,6 @@ function plusSlides(n) {
 function currentSlide(n) {
     showSlides(slideIndex = n);
 }
-
 
 function showSlides(n) {
     //Wrap the next/prev scrolling
@@ -45,22 +40,39 @@ function showSlides(n) {
 }
 
 function setImages(pageIndex) {
-    for(i=0; i<9; i++) {
-        document.getElementsByClassName('thumbnail')[i].getElementsByTagName("img")[0].src = urls[i+(pageIndex-1)*9];
+    for(i=0; i<pageSize; i++) {
+        if ((i+(pageIndex-1)*pageSize)>=urls.length){ //reached end of URL array
+            document.getElementsByClassName('thumbnail')[i].getElementsByTagName("img")[0].src = "";
+        }
+        else {
+            document.getElementsByClassName('thumbnail')[i].getElementsByTagName("img")[0].src = urls[i+(pageIndex-1)*pageSize];
+        }
     }
 }
 
-function initialLoad(){
+function initialLoad(numPages){
+    var dotHTML="";
+    //Generate Dots
+    $("#dots").html(function(n){
+        for (i=0; i<numPages; i++) {
+            dotHTML = dotHTML + "<span class=\"dot\" onclick=\"currentSlide("
+            dotHTML = dotHTML + (i+1);
+            dotHTML = dotHTML + ")\"> </span>";
+        }
+        return dotHTML;
+    });
+
+    var dots = document.getElementsByClassName("dot");
+    dots[0].className += " active";
+    //Generate Row 1
     $("#myrow").html(function(slideIndex){
         return generateRow(1);
     });
-    var dots = document.getElementsByClassName("dot");
-    dots[0].className += " active";
 }
 
 function generateRow(n) {
     var toReturn = "";
-    for (j=n-1; j<9*n; j++) {
+    for (j=n-1; j<pageSize*n; j++) {
         toReturn = toReturn + "<div class=\"col-sm-4 thumbnail\" imgId=\"";
         toReturn = toReturn + j;
         toReturn = toReturn + "\">"
@@ -72,23 +84,38 @@ function generateRow(n) {
     return toReturn;
 }
 
-$(function() {
-    var pageSize = 9;
-    numPages = populateURL();
+function getList(_callback) {
+    //S3 Prep
+    var s3 = new AWS.S3();
+    category = document.getElementById('pagecategory');
+    var prefix = category.attributes.class.value + "/";
+    var albumBucketName = "neil-life";
+    var delimiter = '/';
+    var params = {
+        Bucket: albumBucketName,
+        Delimiter: delimiter,
+        Prefix: prefix
+    }
+    options = { scope : 'profile' };
 
-    var dotHTML = "";
-    //Generate Dots
-    $("#dots").html(function(n){
-        for (i=0; i<numPages; i++) {
-            dotHTML = dotHTML + "<span class=\"dot\" onclick=\"currentSlide("
-            dotHTML = dotHTML + (i+1);
-            dotHTML = dotHTML + ")\"> </span>";
+    //S3 Request
+    s3.listObjects(params, function (err, data) {
+        if(err)throw err;
+        else
+        {
+            for (i = 0; i < data.Contents.length; i++) {
+                urls[i] = "https://d3qe819fh6emdz.cloudfront.net/" + data.Contents[i].Key;
+            }
+            numPages = Math.ceil(urls.length / pageSize);
+            _callback(Math.ceil(urls.length / pageSize));
         }
-        return dotHTML;
     });
+}
 
-    slideIndex = 1;
-    initialLoad();
-
+$(function() {
+    slideIndex=1;
+    getList(function(numPages){
+        initialLoad(numPages);
+    });
 });
 
